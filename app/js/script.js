0d6d9ed0406c5c548bee4f125b867f9c9d824060
Pablo <paceruva24@gmail.com>
2021-01-07 14:14:59 -0500

    Dark theme implemented

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,15 @@
-document.querySelector("#creator").addEventListener("click", function() {
-    document.querySelector("#createNew").focus();
-})
+document.querySelector("#creator").addEventListener("click",
+    function() {
+        document.querySelector("#createNew").focus();
+    })
+
+document.querySelector("#theme-icon").addEventListener("click",
+    function() {
+        let body = document.querySelector("body");
+        if (!body.classList.contains("dark-theme")) {
+            body.classList.add("dark-theme");
+        } else {
+
+            body.classList.remove("dark-theme");
+        }
+    })
